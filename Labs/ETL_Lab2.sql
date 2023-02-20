596d690c0f3edb8e84dce24964a9c9dc9e6ed337
Carson Crenshaw <81333598+C-Crenshaw@users.noreply.github.com>
2023-02-20 12:20:48 -0500

    Add files via upload
    Completed on Thursday, February 16, 2023.

---
+++ Labs/ETL_Lab2.sql
@@ -0,0 +1,303 @@
+# Creating the Dimensional Schema
+CREATE DATABASE `northwind_dw` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE Northwind_dw;
+USE Northwind;
+
+CREATE TABLE `dim_customers` (
+  `customer_key` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `job_title` varchar(50) DEFAULT NULL,
+  `business_phone` varchar(25) DEFAULT NULL,
+  `fax_number` varchar(25) DEFAULT NULL,
+  `address` longtext,
+  `city` varchar(50) DEFAULT NULL,
+  `state_province` varchar(50) DEFAULT NULL,
+  `zip_postal_code` varchar(15) DEFAULT NULL,
+  `country_region` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`customer_key`),
+  KEY `city` (`city`),
+  KEY `company` (`company`),
+  KEY `first_name` (`first_name`),
+  KEY `last_name` (`last_name`),
+  KEY `zip_postal_code` (`zip_postal_code`),
+  KEY `state_province` (`state_province`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `dim_employees` (
+  `employee_key` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
+  `job_title` varchar(50) DEFAULT NULL,
+  `business_phone` varchar(25) DEFAULT NULL,
+  `home_phone` varchar(25) DEFAULT NULL,
+  `fax_number` varchar(25) DEFAULT NULL,
+  `address` longtext,
+  `city` varchar(50) DEFAULT NULL,
+  `state_province` varchar(50) DEFAULT NULL,
+  `zip_postal_code` varchar(15) DEFAULT NULL,
+  `country_region` varchar(50) DEFAULT NULL,
+  `web_page` longtext,
+  PRIMARY KEY (`employee_key`),
+  KEY `city` (`city`),
+  KEY `company` (`company`),
+  KEY `first_name` (`first_name`),
+  KEY `last_name` (`last_name`),
+  KEY `zip_postal_code` (`zip_postal_code`),
+  KEY `state_province` (`state_province`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `dim_products` (
+  `product_key` int NOT NULL AUTO_INCREMENT,
+  `product_code` varchar(25) DEFAULT NULL,
+  `product_name` varchar(50) DEFAULT NULL,
+  `standard_cost` decimal(19,4) DEFAULT '0.0000',
+  `list_price` decimal(19,4) NOT NULL DEFAULT '0.0000',
+  `reorder_level` int DEFAULT NULL,
+  `target_level` int DEFAULT NULL,
+  `quantity_per_unit` varchar(50) DEFAULT NULL,
+  `discontinued` tinyint(1) NOT NULL DEFAULT '0',
+  `minimum_reorder_quantity` int DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`product_key`),
+  KEY `product_code` (`product_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `dim_shippers` (
+  `shipper_key` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) DEFAULT NULL,
+  `address` longtext,
+  `city` varchar(50) DEFAULT NULL,
+  `state_province` varchar(50) DEFAULT NULL,
+  `zip_postal_code` varchar(15) DEFAULT NULL,
+  `country_region` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`shipper_key`),
+  KEY `city` (`city`),
+  KEY `company` (`company`),
+  KEY `zip_postal_code` (`zip_postal_code`),
+  KEY `state_province` (`state_province`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+# Creating Fact Table (From four tables to two, then two to one)
+
+CREATE TABLE `fact_orders` (
+  `fact_order_key` int NOT NULL AUTO_INCREMENT,
+  `order_key` int DEFAULT NULL,
+  `employee_key` int DEFAULT NULL,
+  `customer_key` int DEFAULT NULL,
+  `product_key` int DEFAULT NULL,
+  `shipper_key` int DEFAULT NULL,
+  `ship_name` varchar(50) DEFAULT NULL,
+  `ship_address` longtext,
+  `ship_city` varchar(50) DEFAULT NULL,
+  `ship_state_province` varchar(50) DEFAULT NULL,
+  `ship_zip_postal_code` varchar(50) DEFAULT NULL,
+  `ship_country_region` varchar(50) DEFAULT NULL,
+  `quantity` decimal(18,4) NOT NULL DEFAULT '0.0000',
+  `order_date` datetime DEFAULT NULL,
+  `shipped_date` datetime DEFAULT NULL,
+  `unit_price` decimal(19,4) DEFAULT '0.0000',
+  `discount` double NOT NULL DEFAULT '0',
+  `shipping_fee` decimal(19,4) DEFAULT '0.0000',
+  `taxes` decimal(19,4) DEFAULT '0.0000',
+  `payment_type` varchar(50) DEFAULT NULL,
+  `paid_date` datetime DEFAULT NULL,
+  `tax_rate` double DEFAULT '0',
+  `order_status` varchar(50) NOT NULL,
+  `order_details_status` varchar(50) NOT NULL,
+  PRIMARY KEY (`fact_order_key`),
+  KEY `order_key` (`order_key`),
+  KEY `customer_key` (`customer_key`),
+  KEY `employee_key` (`employee_key`),
+  KEY `product_key` (`product_key`),
+  KEY `shipper_key` (`shipper_key`),
+  KEY `ship_zip_postal_code` (`ship_zip_postal_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4;
+
+TRUNCATE TABLE `northwind_dw`.`fact_orders`;
+
+# Populating the Newly-Created Dimensional Schema
+INSERT INTO northwind_dw.dim_customers
+(customer_key,
+company,
+last_name,
+first_name,
+job_title,
+business_phone,
+fax_number,
+address,
+city,
+state_province,
+zip_postal_code,
+country_region) 
+SELECT id,
+company,
+last_name,
+first_name,
+job_title,
+business_phone,
+fax_number,
+address,
+city,
+state_province,
+zip_postal_code,
+country_region FROM northwind.customers;
+
+INSERT INTO `northwind_dw`.`dim_employees`
+(`employee_key`,
+`company`,
+`last_name`,
+`first_name`,
+`email_address`,
+`job_title`,
+`business_phone`,
+`home_phone`,
+`fax_number`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`,
+`web_page`)
+SELECT `employees`.`id`,
+    `employees`.`company`,
+    `employees`.`last_name`,
+    `employees`.`first_name`,
+    `employees`.`email_address`,
+    `employees`.`job_title`,
+    `employees`.`business_phone`,
+    `employees`.`home_phone`,
+    `employees`.`fax_number`,
+    `employees`.`address`,
+    `employees`.`city`,
+    `employees`.`state_province`,
+    `employees`.`zip_postal_code`,
+    `employees`.`country_region`,
+    `employees`.`web_page`
+FROM `northwind`.`employees`;
+
+INSERT INTO `northwind_dw`.`dim_products`
+(`product_key`,
+`product_code`,
+`product_name`,
+`standard_cost`,
+`list_price`,
+`reorder_level`,
+`target_level`,
+`quantity_per_unit`,
+`discontinued`,
+`minimum_reorder_quantity`,
+`category`)
+SELECT `products`.`id`,
+    `products`.`product_code`,
+    `products`.`product_name`,
+    `products`.`standard_cost`,
+    `products`.`list_price`,
+    `products`.`reorder_level`,
+    `products`.`target_level`,
+    `products`.`quantity_per_unit`,
+    `products`.`discontinued`,
+    `products`.`minimum_reorder_quantity`,
+    `products`.`category`
+FROM `northwind`.`products`;
+
+INSERT INTO `northwind_dw`.`dim_shippers`
+(`shipper_key`,
+`company`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`)
+SELECT `shippers`.`id`,
+    `shippers`.`company`,
+    `shippers`.`address`,
+    `shippers`.`city`,
+    `shippers`.`state_province`,
+    `shippers`.`zip_postal_code`,
+    `shippers`.`country_region`
+FROM `northwind`.`shippers`;
+
+INSERT INTO `northwind_dw`.`fact_orders`
+(`fact_order_key`,
+`order_key`,
+`employee_key`,
+`customer_key`,
+`product_key`,
+`shipper_key`,
+`ship_name`,
+`ship_address`,
+`ship_city`,
+`ship_state_province`,
+`ship_zip_postal_code`,
+`ship_country_region`,
+`quantity`,
+`order_date`,
+`shipped_date`,
+`unit_price`,
+`discount`,
+`shipping_fee`,
+`taxes`,
+`payment_type`,
+`paid_date`,
+`tax_rate`,
+`order_status`,
+`order_details_status`)
+SELECT o.id,
+    o.employee_id,
+    o.customer_id,
+    od.product_id,
+    o.shipper_id,
+    o.ship_name,
+    o.ship_address,
+    o.ship_city,
+    o.ship_state_province,
+    o.ship_zip_postal_code,
+    o.ship_country_region,
+    od.quantity,
+    o.order_date,
+    o.shipped_date,
+    od.unit_price,
+    od.discount,
+    o.shipping_fee,
+    o.taxes,
+    o.payment_type,
+    o.paid_date,
+    o.tax_rate,
+    os.status_name AS order_status,
+    ods.status_name AS order_details_status
+FROM northwind.orders AS o
+INNER JOIN northwind.orders_status AS os
+ON o.status_id = os.id
+RIGHT OUTER JOIN northwind.order_details AS od
+ON o.id = od.order_id
+INNER JOIN northwind.order_details_status AS ods
+ON od.status_id = ods.id;
+
+# Creating a Dimensional Report
+# Selecting Each Customer’s Last Name, the total amount of the order quantity associated with each customer, and the total amount of the order unit price associated with each custome
+SELECT customers.last_name AS `customer_name`,
+    SUM(orders.quantity) AS `total_quantity`,
+    SUM(orders.unit_price) AS `total_unit_price`
+FROM northwind_dw.fact_orders AS orders
+INNER JOIN northwind_dw.dim_customers AS customers
+ON orders.customer_key = customers.customer_key
+GROUP BY customers.last_name
+ORDER BY total_unit_price DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
